03110ebd5031a2c56731cccf68f19a8b2893f826
Luiz Felipe de J. Santos <luiz_felipe_js@hotmail.com>
2022-01-29 08:21:49 -0300

    MongoDB and .env setups

--- serv.js
+++ serv.js
@@ -1,12 +1,58 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("bate-papo-UOL_DB");
+});
 const serv = express();
 serv.use(express.json());
 serv.use(cors());
 
 serv.get("/", (req, res) => {
-    res.send('Hello World');
+    res.send('Server is Running');
 });
 
-serv.listen(5000);
+//POSTS
+
+serv.post("/participants", (req, res) => {
+    res.send('Server is Running');
+});
+
+serv.post("/messages", (req, res) => {
+    res.send('Server is Running');
+});
+
+serv.post("/status", (req, res) => {
+    res.send('Server is Running');
+});
+
+//GETS
+
+serv.get("/participants", (req, res) => {
+    res.send('Server is Running');
+});
+
+serv.get("/messages", (req, res) => {
+    res.send('Server is Running');
+});
+
+//DELETES
+
+serv.delete("/messages/:id", (req, res) => {
+    res.send('Server is Running');
+});
+
+//PUTS
+
+serv.put("/messages/:id", (req, res) => {
+    res.send('Server is Running');
+});
+
+serv.listen(process.env.PORT, () => {
+    console.log('Servidor iniciado em http://localhost:5000')
+  });
